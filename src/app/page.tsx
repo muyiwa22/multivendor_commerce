a6e4bd9577643d30cb5bd3e4042ea981093a8991
adeolu <muyiwaolugbebi@gmail.com>
2025-04-23 22:15:22 +0100

    basic set up, shadcnui, theme-provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import ThemeToggle from "@/components/shared/theme-toggle";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-      <div><h1 className="font-bold text-blue-500">Hello</h1></div>
+      <div className="p-5">
+        <div className="w-full flex justify-end">
+          <ThemeToggle />
+        </div>
+        <h1 className="font-bold text-blue-500 font-barlow">Hello</h1>
+        <Button  variant="destructive">Click Here</Button>
+      </div>
   );
 }
